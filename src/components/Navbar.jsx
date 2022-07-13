6275a20fa2b7c765b4c72f54c4d43642a16cdd74
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-13 20:26:26 +0000

    installed material ui and icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import SearchIcon from '@mui/icons-material/Search';
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Language = styled.span`
+ font-size: 14px;
+ cursor: pointer;
+`;
+
+const SearchContainer = styled.div``;
+
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
 
 const Navbar = () => {
   return (
     <Container>
-      Navbar
+      <Wrapper>
+        <Left>
+            <Language>En</Language>
+            <SearchContainer>
+                <SearchIcon />
+            </SearchContainer>
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Navbar
-
-
-const Container = styled.div``;
+export default Navbar;
