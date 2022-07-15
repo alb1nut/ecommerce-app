9f1eee23b347c329e73a519da975d01220a69cc5
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-15 12:51:51 +0000

    finished product and product components

--- src/data.js
+++ src/data.js
@@ -43,34 +43,34 @@ export const sliderItems = [
   export const popularProducts = [
     {
       id:1,
-      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+      img:"https://www.structuralguide.com/wp-content/uploads/2020/09/hollow-concrete-blocks.jpg",
     },
     {
       id:2,
-      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+      img:"https://en.lesso.com/upfile/2021/04/20210413170018_544.png",
     },
     {
       id:3,
-      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+      img:"https://116kxc3yni1m23llck48qpul-wpengine.netdna-ssl.com/wp-content/uploads/2019/08/PVC-Plumbing-Pipes-Best-Materials-for-Water-Pipes.jpg",
     },
     {
       id:4,
-      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+      img:"http://www.varunpiping.com/images/products/Varun-Rigid-PVC-Pipes-01.jpg",
     },
     {
       id:5,
-      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+      img:"https://www.sweetmelodiesfm.com/wp-content/uploads/2021/11/supply-master-building-material-ghacem-cement-grade-32-5r-and-42-5r-12570477985858_800x.jpg",
     },
     {
       id:6,
-      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+      img:"https://wzppgi.com/wp-content/uploads/2021/08/Wavy-GI-Roofing-Sheet.jpg",
     },
     {
       id:7,
-      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+      img:"https://ghanainsider.com/wp-content/uploads/2020/09/coloured-roofing-sheet-500x500-1.jpg",
     },
     {
       id:8,
-      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+      img:"https://reapp.com.gh/wp-content/uploads/2021/11/B6B1CC92-5B9E-477D-884F-F29684F2C615.jpeg",
     },
   ]
