51855ca55ccb2025a08caa8d71be1d4e597b81bf
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-17 19:18:52 +0000

    installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import Home from "./pages/Home";
-// import Cart from "./pages/Cart";
-// import Login from "./pages/Login";
-// import Product from "./pages/Product";
-// import Register from "./pages/Register";
-// import ProductList from "./pages/ProductList";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import ProductList from "./pages/ProductList";
+import { Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      {/* <ProductList /> */}
-      {/* <Product /> */}
-      {/* <Cart /> */}
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
