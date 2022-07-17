b83019d2c4e0ff3a6c6faf375f26d2e7ee554c9a
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-17 19:32:49 +0000

    added navigation to registerr and login buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-import SearchIcon from '@mui/icons-material/Search';
-import Badge from '@mui/material/Badge';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import SearchIcon from "@mui/icons-material/Search";
+import Badge from "@mui/material/Badge";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import { mobile } from "../responsive";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   height: 60px;
@@ -14,66 +15,63 @@ const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
-  align-items:center;
+  align-items: center;
   ${mobile({ padding: "10px 0px" })}
 `;
 const Left = styled.div`
   flex: 1;
   display: flex;
-  align-items:center;
+  align-items: center;
 `;
 
 const Language = styled.span`
- font-size: 14px;
- cursor: pointer;
- margin-left:25px;
- padding:5px;
- ${mobile({ display: "none" })}
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  padding: 5px;
+  ${mobile({ display: "none" })}
 `;
 
-
-
 const SearchContainer = styled.div`
-border: 0.5px solid lightgray;
-display:flex;
-align-items:center;
-margin-left:25px;
-padding:5px;
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
 
-input{
-    outline:none;
-}
+  input {
+    outline: none;
+  }
 `;
 
 const Input = styled.input`
- border:none;
- ${mobile({ width: "50px" })}
+  border: none;
+  ${mobile({ width: "50px" })}
 `;
 
 const Center = styled.div`
   flex: 1;
-  text-align:center;
+  text-align: center;
 `;
 
-const Logo =styled.h1`
-font-weight:bold;
-${mobile({ fontSize: "24px" })}
+const Logo = styled.h1`
+  font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
 `;
 
 const Right = styled.div`
   flex: 1;
-display:flex;
-justify-content:flex-end;
-align-items:center;
-${mobile({ flex: 2, justifyContent: "center" })}
-
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  ${mobile({ flex: 2, justifyContent: "center" })}
 `;
 
-const MenuItem =styled.div`
-font-size:14px;
-cursor:pointer;
-margin-left:25px;
-${mobile({ fontSize: "12px", marginLeft: "10px" })}
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `;
 
 const Navbar = () => {
@@ -81,23 +79,43 @@ const Navbar = () => {
     <Container>
       <Wrapper>
         <Left>
-            <Language>En</Language>
-            <SearchContainer>
-                <Input />
-                <SearchIcon style={{color:"gray" , fontSize: 16}}/>
-            </SearchContainer>
+          <Language>En</Language>
+          <SearchContainer>
+            <Input />
+            <SearchIcon style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
         </Left>
         <Center>
-            <Logo>BIT</Logo>
+          <Logo>BIT</Logo>
         </Center>
         <Right>
-            <MenuItem >Register</MenuItem>
-            <MenuItem >SignIn</MenuItem>
-            <MenuItem >
+          <MenuItem>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "#000",
+              }}
+              to="/register"
+            >
+              Register
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "#000",
+              }}
+              to="/login"
+            >
+              SignIn
+            </Link>
+          </MenuItem>
+          <MenuItem>
             <Badge badgeContent={4} color="primary">
-            <ShoppingCartOutlinedIcon />
-    </Badge>
-            </MenuItem>
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
         </Right>
       </Wrapper>
     </Container>
