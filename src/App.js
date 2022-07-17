ab86586311a17d115281003bdc9ea05ca08deaed
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-17 18:12:37 +0000

    added media query to navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-// import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import Login from "./pages/Login";
-import Product from "./pages/Product";
-import Register from "./pages/Register";
+import Home from "./pages/Home";
+// import Cart from "./pages/Cart";
+// import Login from "./pages/Login";
+// import Product from "./pages/Product";
+// import Register from "./pages/Register";
 // import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
+      <Home />
       {/* <ProductList /> */}
       {/* <Product /> */}
-      <Cart />
+      {/* <Cart /> */}
     </div>
   );
 }
